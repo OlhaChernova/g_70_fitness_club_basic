b0e86506d68e39b4ac7e13fc8e537846e08f38a8
OlhaChernova1987 <chernova.olha1987@gmail.com>
2025-09-14 13:48:17 +0200

    service implemented

---
+++ src/main/java/app/service/MemberService.java
@@ -0,0 +1,85 @@
+package app.service;
+
+import app.domain.Member;
+import app.exceptions.MemberRestoreException;
+import app.exceptions.MemberSaveException;
+import app.exceptions.MemeberNotFoundException;
+import app.repository.MemberRepository;
+
+import java.util.List;
+
+public class MemberService {
+
+    private final MemberRepository repository = new MemberRepository();
+    private static MemberService instance;
+
+    private MemberService() {
+    }
+
+    public static MemberService getInstance() {
+        if (instance == null) {
+            instance = new MemberService();
+        }
+        return instance;
+    }
+
+    public Member save(Member member) {
+        if (member == null) {
+            throw new MemberSaveException("Пользователь не может быть пустым");
+        }
+
+        String name = member.getName();
+        if (name == null || name.trim().isEmpty()) {
+            throw new MemberSaveException("Имя не может быть пустым");
+        }
+
+        member.setActive(true);
+        return repository.save(member);
+    }
+
+    public List<Member> getAllActiveMember() {
+        return repository.findAll()
+                .stream()
+                .filter(Member::isActive)
+                .toList();
+    }
+
+    public Member getActiveMemberById(Long id) {
+        Member member = repository.findById(id);
+
+        if (member == null || !member.isActive()) {
+            throw new MemeberNotFoundException(id);
+        }
+        return member;
+    }
+
+    public void deleteById(Long id) {
+        Member member = getActiveMemberById(id);
+        member.setActive(false);
+    }
+
+    public void deleteByName(String name) {
+        getAllActiveMember()
+                .stream()
+                .filter(x -> x.getName().equals(name))
+                .forEach(x -> x.setActive(false));
+    }
+
+    public void restoreById(Long id) {
+        Member member = repository.findById(id);
+
+        if (member == null) {
+            throw new MemeberNotFoundException(id);
+        }
+        member.setActive(true);
+    }
+
+    public void restoreByName(String name) {
+
+        if (name == null || name.trim().isEmpty()) {
+            throw new MemberRestoreException("Имя не может быть пустым");
+        }
+        Member member = repository.findByName(name);
+        member.setActive(true);
+    }
+}
